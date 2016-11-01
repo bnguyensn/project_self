f2f984ba72cec94df617cddb8625c60ab98a7fc8
Binh Nguyen <b.nguyen.sn@gmail.com>
2016-11-01 23:03:37 +0000

    Updated homepage.

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    var method = req.method;
+    var url = req.url;
+    var headers = req.headers;
+    var userAgent = headers['user-agent'];
+
+    res.render('index', {title: 'Just A Reever'});
 });
 
 module.exports = router;
