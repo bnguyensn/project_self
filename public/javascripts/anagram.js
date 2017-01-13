8fb9ac508c3a6d602119f104333aec54ad64d659
Binh Nguyen <b.nguyen.sn@gmail.com>
2017-01-13 21:45:51 +0000

    Anagram - minor update  - Minor updates to anagram's view and viewmodel.

--- public/javascripts/anagram.js
+++ public/javascripts/anagram.js
@@ -1,9 +1,37 @@
 $(document).ready(function() {
 
+    // View Model - Handled by vuejs
+
     let app = new Vue({
         el: '#anagram-app',
         data: {
-            message: 'Phrase to be solved...'
+            queryMsg: 'Enter a phrase...',
+            resultMsg: 'Phrase to be solved:',
+        },
+        methods: {
+            // Quality-of-life query input box stuff
+            queryInputClicked: function(e) {
+                let $e = $(e.currentTarget);
+                // $e.select(); // Either select everything in the input form, or delete everything.
+                this.queryMsg = '';
+                console.log($e.attr('id') + 'clicked')
+            },
+
+            // This method is run right after the user clicks the "Solve!" button.
+            queryAnagram: function (e) {
+                test(this.queryMsg);
+                this.resultMsg = 'Solving: ' + this.queryMsg;
+            }
         }
-    })
+    });
+
+    // Model - Anagram Solving Logic
+
+    function test(t) {
+        if(t.length != 0) {
+            console.log('Solving phrase ' + t)
+        } else {
+            console.log('Make sure the input field is not empty!')
+        }
+    }
 });
